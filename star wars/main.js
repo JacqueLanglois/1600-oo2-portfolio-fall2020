9cf76e481ee90408996faf20bb54e6a5c33f47ff
Jacque Langlois <10820557@uvu.edu>
2020-11-04 00:26:57 -0700

    added CSS to movie list

--- star wars/main.js
+++ star wars/main.js
@@ -1,34 +1,20 @@
 import { films } from './Data/films.js'
-import { species } from '.Data/species.js'
+import { species } from './Data/species.js'
 
 //console.log(species.length)
 
 const main = document.querySelector('main')
 
-/*films.forEach(film => {
-    console.log(film.title)
-    let newTitle = main.appendChild(document.createElement("h1"))
-    newTitle.textContent = film.title
-}); */
-
-/*species.forEach(person => {console.log(species.name)
-    let personImg = document.createElement('img')
-    personImg.src = ""
-    main.appendChild(personImg)
-
-}) */
-
-//https://starwars-visualguide.com/assets/img/films/1.jpg
-
-const main = document.querySelector('main')
-
-for (let i = 0; < 7; i++) {
+for (let i = 0; i < 7; i++) {
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = 'https://starwars-visualguide.com/assets/img/films/' + (i + 1) + '.jpg'
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = 'Movie Title Here'
+    figCaption.textContent = films [i].title
 
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
-}
+
+    main.appendChild(figure)
+} 
+
