7b1f85b168c466bd21cda06f99956a042b55d373
Jacque Langlois <10820557@uvu.edu>
2020-12-16 23:20:29 -0700

    senator page

---
+++ congress/index.js
@@ -0,0 +1,8 @@
+import { senators } from '../Data/senators.js'
+
+const senGrid = document.querySelector('.senGrid')
+const seniorButton = document.querySelector('#seniorityButton')
+const birthdayButton = document.querySelector('#birthdayButton')
+
+
+  
