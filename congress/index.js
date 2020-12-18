f366019dacd57e6dddac48d724467d7851955d02
Jacque Langlois <10820557@uvu.edu>
2020-12-17 18:57:29 -0700

    Fixed it for real this time

--- congress/index.js
+++ congress/index.js
@@ -5,34 +5,39 @@ const senGrid = document.querySelector('.senGrid')
 const seniorButton = document.querySelector('#seniorityButton')
 const birthdayButton = document.querySelector('#birthdayButton')
 
+seniorButton.addEventListener('click', () => {
+    senioritySort()
+})
+
 birthdayButton.addEventListener('click', () => {
     birthdaySort()
 })
 
-  function populateSenatorDiv(simpleSenators) {
-      removeChildren(senGrid)
-      simpleSenators.forEach(senator => {
-          let senDiv = document.createElement('div')
-          let senFigure = document.createElement('figure')
-          let figImg = document.createElement('img')
-          let figCaption = document.createElement('figCaption')
-          let partyIcon = document.createElement('i')
-          if (senator.party === 'R') partyIcon.className = 'fas fa-republican'
-          if (senator.party === 'D') partyIcon.className = 'fas fa-democrat'
-          if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
-          figImg.src = senator.imgURL
-          figCaption.textContent = senator.name 
+function populateSenatorDiv(simpleSenators) {
+    removeChildren(senGrid)
+    simpleSenators.forEach(senator => {
+        let senDiv = document.createElement('div')
+        let senFigure = document.createElement('figure')
+        let figImg = document.createElement('img')
+        let figCaption = document.createElement('figCaption')
+        let partyIcon = document.createElement('i')
+        if (senator.party === 'R') partyIcon.className = 'fas fa-republican'
+        if (senator.party === 'D') partyIcon.className = 'fas fa-democrat'
+        if (senator.party === 'ID') partyIcon.className = 'fas fa-star'
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
 
-          figCaption.appendChild(partyIcon)
-          senFigure.appendChild(figImg)
-          senFigure.appendChild(senFigure)
-          senGrid.appendChild
-      })
-  }
-   
-  function getSimplifiedSenators(senatorArray) {
+        figCaption.appendChild(partyIcon)
+        senFigure.appendChild(figImg)
+        senFigure.appendChild(figCaption)
+        senDiv.appendChild(senFigure)
+        senGrid.appendChild(senDiv)
+    })
+}
+
+function getSimplifiedSenators(senatorArray) {
     return senatorArray.map(senator => {
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+        let middleName = senator.middleName ? `${senator.middleName}` : ` `
         return {
             id: senator.id,
             name: `${senator.first_name}${middleName}${senator.last_name}`,
@@ -41,19 +46,25 @@ birthdayButton.addEventListener('click', () => {
             missedVotesPct: senator.missed_votes_pct,
             party: senator.party,
             loyaltyPct: senator.votes_with_party_pct,
-            date_of_birth: senator.date_of_birth
+            date_of_birth: parseInt(senator.date_of_birth, 10)
         }
     })
-  }
+}
 
-  const mostSeniority = getSimpLifiedSenators(senators).reduce((acc, senators) => acc.seniority > senator.senator.seniority ? acc : senator)
+const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
 
-  function birthdaySort() {
-      populateSenatorDiv(getSimpLifiedSenators(senators).sort((a, b) => {
-          return a.seniority - b.seniority
-      }))
-  }
+function birthdaySort() {
+    populateSenatorDiv(getSimplifiedSenators(senators).sort((a, b) => {
+        return a.date_of_birth - b.date_of_birth
+    }))
+}
 
-  console.log(mostSeniority)
+function senioritySort() {
+    populateSenatorDiv(getSimplifiedSenators(senators).sort((a, b) => {
+        return a.seniority - b.seniority
+    }))
+}
 
-  populateSenatorDiv(getSimpLifiedSenators(senators))
+console.log(mostSeniority)
+
+populateSenatorDiv(getSimplifiedSenators(senators))
